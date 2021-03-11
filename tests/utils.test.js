e29828aa3d3d2dc073951899676b7e12f1cc266c
AvishkaDeSilva <61880105+AvishkaDeSilva@users.noreply.github.com>
2021-03-11 14:36:50 +0530

    UPDATE - utilis.test.js

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -27,25 +27,28 @@ describe('Password Generator', () => {
 
 describe('Password Validator', () => {
     test('should contain a numeric', () => {
-        var password = password_generator(20);
-        expect(password.split('')).to.contain.oneOf('0123456789'.split(''));
-        var newPassword = password.split('').filter((ch) => '0123456789'.split('').indexOf(ch) === -1).join('');
-        const { isValid, msg } = validatePassword(newPassword);
+        var password = validatePassword("AsssgdhjA@jhshjsj");
+        expect(password.msg).to.eql("Error: password must contain at least one number (0-9)!");
     });
     test('should contain a lowercase', () => {
-        var password = password_generator();
-        expect(password.split('')).to.contain.oneOf("abcdefghijklmnopqrstuvwxyz".split(''));
+        var password = validatePassword("1ASSDFXSDDSDFFDSFD@");
+        expect(password.msg).to.eql("Error: password must contain at least one lowercase letter (a-z)!");
     });
     test('should contain a uppercase', () => {
-        var password = password_generator();
-        expect(password.split('')).to.contain.oneOf("abcdefghijklmnopqrstuvwxyz".split('').map(ch => ch.toUpperCase()));
+        var password = validatePassword("1ssdfsdfsfsfsgdgsdd@");
+        expect(password.msg).to.eql("Error: password must contain at least one uppercase letter (A-Z)!");
     });
     test('should contain a special character', () => {
-        var password = password_generator(15);
-        expect(password.split('')).to.contain.oneOf('!@#$%^&*()_+~`}{?><,.-='.split(''));
+        var password = validatePassword("AdadadasaASDadadd1");
+        expect(password.msg).to.eql("Error: password must contain at least one special character!");
     });
     test('should contain at least 10 characters', () => {
-        var password = password_generator(5);
-        expect(password).to.have.lengthOf.at.least(10);
+        var password = validatePassword("Asd1@");
+        expect(password.msg).to.eql("Password must have at least 10 characters");
+    });
+
+    test('Valid Password', () => {
+        var password = validatePassword("Asd1@dddgsgseegg");
+        expect(password.msg).to.eql("Valid password!");
     });
 });
